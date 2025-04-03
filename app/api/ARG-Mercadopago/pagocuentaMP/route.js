cf9d1a92c92c94310a36ccf09517e0c565f56178
Draftory <draftoryhq@gmail.com>
2025-04-03 11:23:24 -0300

    CORS

--- app/api/ARG-Mercadopago/pagocuentaMP/route.js
+++ app/api/ARG-Mercadopago/pagocuentaMP/route.js
@@ -1,4 +1,4 @@
-// app/api/create-preference-wallet-pro/route.js
+// app/api/create-preference-pro/route.js
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { NextResponse } from 'next/server';
 
@@ -7,73 +7,67 @@ const allowedOrigins = [
   'https://inmoacuerdos.webflow.io',
 ];
 
-export async function OPTIONS(request) {
-  const origin = request.headers.get('origin');
+export async function OPTIONS(req) {
+  const origin = req.headers.get('origin');
+  const headers = {
+    'Access-Control-Allow-Origin': allowedOrigins.includes(origin)
+      ? origin
+      : allowedOrigins[0],
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  };
 
-  if (allowedOrigins.includes(origin)) {
-    return new NextResponse(null, {
-      status: 204,
-      headers: {
-        'Access-Control-Allow-Origin': origin || '*',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
-  }
-
-  return new NextResponse(null, { status: 405, body: 'Method Not Allowed' });
+  return new NextResponse(null, {
+    status: 204,
+    headers: headers,
+  });
 }
 
-export async function POST(request) {
-  const origin = request.headers.get('origin');
-
-  if (!allowedOrigins.includes(origin)) {
-    return new NextResponse(JSON.stringify({ error: 'Not allowed origin' }), {
-      status: 403,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+export async function POST(req) {
+  console.log('Starting API request to create Mercado Pago Preference');
+  const origin = req.headers.get('origin');
+  const headers = {
+    'Access-Control-Allow-Origin': allowedOrigins.includes(origin)
+      ? origin
+      : allowedOrigins[0],
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  };
 
   try {
-    const req = await request.json();
+    const requestBody = await req.json();
     const client = new MercadoPagoConfig({ accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN });
     const preference = new Preference(client);
 
     const preferenceResult = await preference.create({
       body: {
-        payment_methods: {
-          excluded_payment_methods: [],
-          excluded_payment_types: [
-            {
-              id: "ticket"
-            }
-          ],
-          installments: 1
-        },
         items: [
           {
-            title: req.title,
-            quantity: Number(req.quantity),
-            unit_price: Number(req.price),
+            title: requestBody.title,
+            quantity: Number(requestBody.quantity),
+            unit_price: Number(requestBody.price),
             currency_id: 'ARS',
-          }
+          },
         ],
         back_urls: {
           success: 'https://www.inmoacuerdos.com/pago-exitoso', // Reemplaza con tu URL de éxito
           failure: 'https://www.inmoacuerdos.com/pago-fallido', // Reemplaza con tu URL de fallo
-          pending: 'https://www.inmoacuerdos.com/pago-pendiente', // Reemplaza con tu URL de pendiente (opcional)
+          pending: 'https://www.inmoacuerdos.com/pago-pendiente', // Reemplaza con tu URL de pendiente
         },
         auto_return: 'approved',
         notification_url: 'https://inmoacuerdos-vercel-server.vercel.app/api/ARG-Mercadopago/webhook-mercado-pago', // Reemplaza con tu URL de webhook
-      }
+      },
     });
 
-    return NextResponse.json({ preferenceId: preferenceResult.id });
+    return new NextResponse(JSON.stringify({ init_point: preferenceResult.init_point }), {
+      status: 200,
+      headers: { ...headers, 'Content-Type': 'application/json' },
+    });
   } catch (error) {
     console.error('Error al crear la preferencia:', error);
     return new NextResponse(JSON.stringify({ error: 'Error al crear la preferencia de pago' }), {
       status: 500,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { ...headers, 'Content-Type': 'application/json' },
     });
   }
 }
