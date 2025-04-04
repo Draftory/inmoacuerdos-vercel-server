91896039ff59a7c46321c9730471fdaa322f9fd9
Draftory <draftoryhq@gmail.com>
2025-04-04 15:39:45 -0300

    +

--- app/api/ARG-Mercadopago/webhook-mercado-pago/route.js
+++ app/api/ARG-Mercadopago/webhook-mercado-pago/route.js
@@ -1,8 +1,17 @@
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const payment = req.query;
-    console.log({ payment });
-    res.status(200).json({ received_via_query: true, data: payment });
+    const { 'data.id': dataId, type } = req.query;
+
+    console.log('Webhook Recibida (GET con parámetros):');
+    console.log({ dataId, type });
+
+    // Aquí puedes procesar la información
+    if (type === 'payment') {
+      console.log(`ID de pago recibido: ${dataId}`);
+      // Agrega aquí tu lógica para el evento de pago
+    }
+
+    res.status(200).json({ received: true, method: 'GET', query_params: req.query });
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
