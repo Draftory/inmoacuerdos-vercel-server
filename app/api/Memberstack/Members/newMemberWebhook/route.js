0e9f8f19809d7299f8bdaaf8d7698acd80e497f2
Draftory <draftoryhq@gmail.com>
2025-04-11 11:41:53 -0300

    errors

--- app/api/Memberstack/Members/newMemberWebhook/route.js
+++ app/api/Memberstack/Members/newMemberWebhook/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
 
     const memberstackId = memberstackData.payload?.id;
     const email = memberstackData.payload?.auth?.email;
-    const name = memberstackData.payload?.customFields?.name;
+    const name = memberstackData.payload?.customFields?.['first-name']; // Corrected line
 
     console.log('Extracted Data:', { memberstackId, email, name });
 
