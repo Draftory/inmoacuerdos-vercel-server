7a148411791fdfcb7e794834d6d896d4faea5bcd
Draftory <draftoryhq@gmail.com>
2025-04-24 10:12:19 -0300

    Resend

---
+++ app/api/Resend/email-one-time-purchase/route.js
@@ -0,0 +1,48 @@
+// app/api/test-email/route.js
+import { Resend } from "resend";
+import { NextResponse } from "next/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req) {
+  try {
+    const { to, subject, body } = await req.json();
+
+    if (!to || !subject || !body) {
+      return NextResponse.json(
+        { error: "Faltan parámetros: to, subject o body." },
+        { status: 400 }
+      );
+    }
+
+    const data = await resend.emails.send({
+      from: "tu_direccion_verificada@inmoacuerdos.com", // Reemplaza con tu dirección verificada en Resend
+      to: to,
+      subject: subject,
+      html: `<p>${body}</p>`,
+    });
+
+    return NextResponse.json(
+      { message: "Correo electrónico enviado exitosamente!", data },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error al enviar el correo electrónico:", error);
+    return NextResponse.json(
+      {
+        error: "Error al enviar el correo electrónico con Resend.",
+        details: error,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req) {
+  return NextResponse.json(
+    {
+      message: "Este endpoint solo acepta solicitudes POST para enviar emails.",
+    },
+    { status: 405 }
+  );
+}
